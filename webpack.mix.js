a1c31ebd875879b0192fe3c41bf7632db279d331
Nathan Heffley <nathanheffley@gmail.com>
2017-11-16 21:01:18 -0500

    Install laravel-elm

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+let elm = require('laravel-elm');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +13,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .then(elm);
